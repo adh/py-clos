4298127343aa24d7af9e272d98d13f67a54f7dfb
Ales Hakl <ales@hakl.net>
2017-01-17 06:15:22 +0100

    use frame inspection instead of traversing sys.modules.

--- py_clos/decorators.py
+++ py_clos/decorators.py
@@ -1,19 +1,31 @@
 from . import base
 from . import util
+import sys
 
-def generic(proc=None, qualifiers=[], function=None, module=None, **kwargs):
-    def wrapper(proc):
+# This is somewhat horrible hack based on inspecting sys._getframe,
+# but it ssems to be best way to implement this without having
+# repeated or redundant nonsencial function names in user code. In
+# contrast to inspecting module namespace or keeping global registry
+# this perfectly matches behavior of normal python scoping rules.
+
+def generic(proc=None, qualifiers=[], frame=None,
+            function=None, module=None, **kwargs):
+    def wrapper(proc, frame=None):
+        if frame is None:
+            frame = sys._getframe(1)
+            
         gf = function
         
         if gf is None:
             mod = module
-            if mod is None:
-                mod = proc.__module__
-            try:
-                gf = util.lookup_object(mod, proc.__qualname__)
-            except:
-                pass
-            
+            if mod is not None:
+                gf = util.lookup_object(mod, proc.__name__)
+            else:
+                try:
+                    gf = frame.f_locals[proc.__name__]
+                except KeyError:
+                    pass
+                
         if gf is None:
             gf = base.GenericFunction(proc.__module__ + '.' + proc.__qualname__)
         
@@ -24,13 +36,14 @@ def generic(proc=None, qualifiers=[], function=None, module=None, **kwargs):
         return gf
 
     if proc:
-        return wrapper(proc)
+        return wrapper(proc, frame=sys._getframe(1))
     else:
         return wrapper
 
 def qualified_generic(qualifiers):
     def wrap(proc=None, **kwargs):
         return generic(proc=proc,
+                       frame=sys._getframe(1),
                        qualifiers=qualifiers,
                        **kwargs)
     return wrap
