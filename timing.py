5552b6d0960fa9bc4f7baf28831d2e9a8f41894b
Ales Hakl <ales@hakl.net>
2017-01-17 13:50:22 +0100

    apples to apples timing

--- timing.py
+++ timing.py
@@ -2,19 +2,23 @@ import time
 from example import *
 
 start = time.time()
-for i in range(10000):
+for i in range(100000):
     display(User(), Screen())
     display(User(), Webpage())
-    get_path(User())
 print(time.time() - start)
 
-def foo():
+class A:
+    def foo(self):
+        return 'a'
+class B(A):
     pass
+
+a = A()
+b = B()
 
 start = time.time()
 for i in range(100000):
-    foo()
-    foo()
-    foo()
+    a.foo()
+    b.foo()
     
 print(time.time() - start)
